943a61f89ce560581c3acbcc91206df1b36cbb51
Ramkrishna Roy <76205677+Ramkrishna307@users.noreply.github.com>
2023-04-02 12:01:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,5 +7,9 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
+let team = players; // creating reference to players array
+let team1 = [...players]; // creating a copy of players array
+
+let cap1 = { ...person }; // creating a copy of person object
 
 export {players,person,team,team1,cap1}
